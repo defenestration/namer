b957ec788b4ccb82250a6d787aac633517f9fd67
Alan B <a.brevick@techsmith.com>
2023-04-10 14:13:57 -0400

    add weapons

--- namer.js
+++ namer.js
@@ -7,11 +7,17 @@ async function go() {
   animals = (await animals.text()).split('\n');
   let vehicles = await fetch("https://raw.githubusercontent.com/defenestration/namer/main/vehicles.txt");
   vehicles = (await vehicles.text()).split('\n');
-  
-  let animal = animals[Math.floor(Math.random()*animals.length)];
-  let vehicle = vehicles[Math.floor(Math.random()*vehicles.length)];
-  str = animal + " " + vehicle;
+  let weapons = await fetch("https://raw.githubusercontent.com/defenestration/namer/main/weapons.txt");
+  weapons = (await weapons.text()).split('\n');
+
+
+
+  let left = animals;
+  let right = vehicles.concat(weapons);
+  str = left[Math.floor(Math.random() * left.length)]
+    + " "
+    + right[Math.floor(Math.random() * right.length)];
 
   console.log(str);
-  showText( str);
+  showText(str);
 }
