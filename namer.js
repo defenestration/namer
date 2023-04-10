7e3e6d231fdbc25636a2d98fb4884d20d56b8fa1
Alan B <a.brevick@techsmith.com>
2023-04-10 17:11:25 -0400

    toggle weapons in js

--- namer.js
+++ namer.js
@@ -1,19 +1,35 @@
 function showText(text) {
   document.getElementById("content").innerHTML = text;
+  document.getElementById("weapon_status").innerHTML = show_weapons;
+}
+
+async function getList(list){
+  let data = await fetch(list);
+  data = (await data.text()).split('\n');
+  return data
+}
+
+//globals
+var animals = getList("https://raw.githubusercontent.com/defenestration/namer/main/animals.txt");
+var weapons = getList("https://raw.githubusercontent.com/defenestration/namer/main/weapons.txt");
+var vehicles = getList("https://raw.githubusercontent.com/defenestration/namer/main/vehicles.txt");
+var show_weapons = true;
+
+function toggle_weapons(){
+  show_weapons = !show_weapons;
+  console.log("toggle_weapons: " + show_weapons);
+  document.getElementById("weapon_status").innerHTML = show_weapons;
 }
 
 async function go() {
-  let animals = await fetch("https://raw.githubusercontent.com/defenestration/namer/main/animals.txt");
-  animals = (await animals.text()).split('\n');
-  let vehicles = await fetch("https://raw.githubusercontent.com/defenestration/namer/main/vehicles.txt");
-  vehicles = (await vehicles.text()).split('\n');
-  let weapons = await fetch("https://raw.githubusercontent.com/defenestration/namer/main/weapons.txt");
-  weapons = (await weapons.text()).split('\n');
-
-
-
-  let left = animals;
-  let right = vehicles.concat(weapons);
+  //await the globals
+  let left = await animals;
+  let ve = await vehicles;
+  let we = await weapons;
+  let right = ve.concat( we );
+  if ( !show_weapons ){
+    right = ve;
+  }
   str = left[Math.floor(Math.random() * left.length)]
     + " "
     + right[Math.floor(Math.random() * right.length)];
